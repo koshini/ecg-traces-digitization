aad41a48b6a55f2564a57d9e07b1ec835859a05b
stephan.z <yuankangzhang01@gmail.com>
2018-10-25 22:10:49 -0400

    Step 4 and a bit step 3

---
+++ README.txt
@@ -0,0 +1,11 @@
+CISC/CMPE 457 Assignment 2
+
+Student 1
+Name: Shini Ko
+Student Number:10153214
+NetID: 14sk22
+
+Student 2
+Name: Yuankang Zhang
+Student Number: 10154776
+NetID: 14yz28
--- main.py
+++ main.py
@@ -29,7 +29,7 @@ translate = (0.0, 0.0)  # amount by which to translate images
 # Image
 
 imageDir = 'images'
-imageFilename = 'ecg-02.png'
+imageFilename = 'ecg-01.png'
 imagePath = os.path.join(imageDir, imageFilename)
 
 image = None  # the image as a 2D np.array
@@ -116,17 +116,53 @@ def compute():
     print '3. removing low-magnitude components'
     threshold = max * 0.4
     gridImageFT[magnitude < threshold] = 0
+    print gridImageFT
 
     if gridImageFT is None:
         gridImageFT = np.zeros((height, width), dtype=np.complex_)
+
+    #store coordinates of points with non-zero maginitude
+    non_zeros = []
+    rows = gridImageFT.shape[0]
+    columns = gridImageFT.shape[1]
+    for x in range(0,rows):
+        for y in range(0, columns):
+            if gridImageFT[x, y] != 0:
+                non_zeros.append([x, y])
 
     # Find (angle, distance) to each peak
     #
     # lines = [ (angle1,distance1), (angle2,distance2) ]
 
-    lines = [[1, 2], [3, 4]]
-
     print '4. finding angles and distances of grid lines'
+
+    #cluster the graph into A, B, C, D zones
+    mid_coordinate = [rows/2, columns/2]
+    a_zone_x,a_zone_y, b_zone_x, b_zone_y, a_fit, b_fit = [], [], [], [], [], []
+
+    for point in non_zeros:
+        #locates points in A zone
+        if point[0] <= mid_coordinate[0] and point[1] <= mid_coordinate[1] and point[1] != 0:
+            a_zone_x.append(point[0])
+            a_zone_y.append(point[1])
+        #locates points in B zone
+        elif point[0] >= mid_coordinate[0] and point[1] <= mid_coordinate[1]:
+            b_zone_x.append(point[0])
+            b_zone_y.append(point[1])
+    if all(i == 0 for i in a_zone_x):
+        a_fit.append(math.tan(3.1415/2))
+        b_fit = np.polyfit(b_zone_x, b_zone_y, 1)
+    else:
+        a_fit = np.polyfit(a_zone_x, a_zone_y, 1)
+        b_fit = np.polyfit(b_zone_x, b_zone_y, 1)
+
+    angle1 = 90 - (math.atan(a_fit[0])) * 180 / 3.1415
+    distance1 = math.sqrt(abs(a_zone_x[1]-a_zone_x[2])**2 + abs(a_zone_y[1]-a_zone_y[2])**2)
+    
+    angle2 = 90 - (math.atan(b_fit[0])) * 180 / 3.1415
+    distance2 = math.sqrt(abs(b_zone_x[1]-b_zone_x[2])**2 + abs(b_zone_y[1]-b_zone_y[2])**2)
+
+    lines = [[angle1, distance1], [angle2, distance2]]
 
     # Convert back to spatial domain to get a grid-like image
 
@@ -705,7 +741,7 @@ def mouseMotion(x, y):
     glutPostRedisplay()
 
 
-# For an image coordinate, if it's < 0 or >= max, wrap the coorindate
+# For an image coordinate, if it's < 0 or >= max, wrap the coordinate
 # around so that it's in the range [0,max-1].  This is useful dealing
 # with FT images.
 
@@ -716,7 +752,6 @@ def wrap(val, max):
         return val - max
     else:
         return val
-
 
 # Load initial data
 #
@@ -793,3 +828,4 @@ else:
 
     glutMainLoop()
     print "done running openGL"
+
